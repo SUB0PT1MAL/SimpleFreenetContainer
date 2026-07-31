b5e70b98f796a9feef39b4a19cc90c8cbce9958e
SUB0PT1MAL <92884665+SUB0PT1MAL@users.noreply.github.com>
2026-07-31 17:06:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,16 @@ case "$1" in
     exec bash "$@"
     ;;
 esac
- 
-# Fordward flags at startup
-exec freenet "$@"
- 
+
+WS_API_ADDRESS="${WS_API_ADDRESS:-0.0.0.0}"
+WS_API_PORT="${WS_API_PORT:-7509}"
+
+# FREENET_EXTRA_ARGS: anything else you want to pass, as one string.
+# ex:  FREENET_EXTRA_ARGS="--some-mode-flag --another-flag value"
+read -r -a EXTRA_ARGS <<< "${FREENET_EXTRA_ARGS:-}"
+
+exec freenet \
+  --ws-api-address "$WS_API_ADDRESS" \
+  --ws-api-port "$WS_API_PORT" \
+  "${EXTRA_ARGS[@]}" \
+  "$@"
